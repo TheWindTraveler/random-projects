45bdcbcc91ba6d69ca8f5034d0b3a2def9031a5c
Begotten-Traveler <solomondev002@gmail.com>
2025-04-22 23:16:17 +0100

    add: Melon fruit

--- projects/fruitSelector.js
+++ projects/fruitSelector.js
@@ -1,5 +1,6 @@
 let boxEl1 = document.getElementById("box1");
 let boxEl2 = document.getElementById("box2");
+let boxEl3 = document.getElementById("box3");
 
 let differentFruits = ["🍎", "🍎",
     "🍉", "🍈", "🍉", "🍈", "🍉", "🍈"
@@ -9,8 +10,10 @@ function displayDifferentFruits(){
     for (let i = 0; i < differentFruits.length; i++){
         if (differentFruits[i] === "🍎" ){
             boxEl1.textContent += "🍎"; 
-        } else if (differentFruits[i] == "🍉" ){
+        } else if (differentFruits[i] === "🍉" ){
             boxEl2.textContent += "🍉";
+        } else if (differentFruits[i] === "🍈"){
+            boxEl3.textContent += "🍈";
         }
     }
 }
