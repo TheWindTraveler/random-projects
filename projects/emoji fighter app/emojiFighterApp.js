24dae93ec60cb1c1d2d424fb36df1aafef583e73
Begotten-Traveler <solomondev002@gmail.com>
2025-04-24 02:07:17 +0100

    add: DOM and array

--- projects/emoji fighter app/emojiFighterApp.js
+++ projects/emoji fighter app/emojiFighterApp.js
@@ -1,1 +1,8 @@
-console.log("Yo! Enoji Fighter Here");
+let fighterEl1 = document.getElementById("fighter1");
+let fighterEl2 = document.getElementById("fighter2");
+
+let fighters = [
+    "🦁", "🐒", "🦍", "🐈‍⬛",
+    "🐅", "🦒", "🦓", "🦄",
+    "🐃", "🐻", "🐍"
+];
