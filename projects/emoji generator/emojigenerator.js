b0eb4c7169c5aa7315bb56ad3cd3c54e3e8061d0
Begotten-Traveler <solomondev002@gmail.com>
2025-04-27 02:35:39 +0100

    chore: JS completed and cursor style updated

--- projects/emoji generator/emojigenerator.js
+++ projects/emoji generator/emojigenerator.js
@@ -9,4 +9,17 @@ let randomEmojiBox2 = document.getElementById("random-emoji-box-2");
 let emojiLength = 5;
 let generateEmoji = document.getElementById("generate-emoji");
 
-console.log(emojis);
+generateEmoji.addEventListener("click", function(){
+    let randomEmoji1 = "";
+    let randomEmoji2 = "";
+    for (let i = 0; i < emojiLength; i++){
+        let randomEmojiIndex1 = Math.floor(Math.random() * emojis.length)
+        let randomEmojiIndex2 = Math.floor(Math.random() * emojis.length)
+        randomEmoji1 += emojis[randomEmojiIndex1];
+        randomEmoji2 += emojis[randomEmojiIndex2];
+    }
+
+    randomEmojiBox1.textContent = randomEmoji1;
+    randomEmojiBox2.textContent = randomEmoji2;
+ 
+})
