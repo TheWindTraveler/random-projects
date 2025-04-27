fd6d3733a35952f54111bdcb71ed7ca2aa7d27b1
Begotten-Traveler <solomondev002@gmail.com>
2025-04-27 02:11:16 +0100

    chore: JS with emoji arrays, length

--- projects/emoji generator/emojigenerator.js
+++ projects/emoji generator/emojigenerator.js
@@ -1,1 +1,12 @@
-console.log("Yo! I'm Linked");
+const emojis = [
+    "😀", "😁", "☺️", "😙", "😘", "🥰", "😍", "😌", "😉", "😭", "😤", "😨", "😱", "😶‍🌫️", "🥶", "🥵", "😳", "🤯", 
+    "🤬", "🤩", "🥳", "☠️", "🤕", "🤒", "💀", "👻", "😻", "💩", "😽", "👊", "🤛", "👁️", "👀", "🫁", "🫂"
+];
+
+
+let randomEmojiBox1 = document.getElementById("random-emoji-box-1");
+let randomEmojiBox2 = document.getElementById("random-emoji-box-2");
+let emojiLength = 5;
+let generateEmoji = document.getElementById("generate-emoji");
+
+console.log(emojis);
