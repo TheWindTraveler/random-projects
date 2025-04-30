8dd12edeb8b95001ed081bd9c2a7d2cbd8796b70
Begotten-Traveler <solomondev002@gmail.com>
2025-04-30 22:32:23 +0100

    chore: popup added

--- projects/click to expand app/clicktoexpand.js
+++ projects/click to expand app/clicktoexpand.js
@@ -1,2 +1,11 @@
 let buttonEl1 = document.getElementById("btn-1");
 let buttonEl2 = document.getElementById("btn-2");
+let popup = document.getElementById("popup")
+
+buttonEl1.addEventListener("click", function(){
+    popup.style.display = "block";
+})
+
+function closePopup(){
+    popup.style.display = "none";
+}
