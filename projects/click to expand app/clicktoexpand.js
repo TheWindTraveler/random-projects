f5f3ca240bd140d36735e9b4165e05966c8ced6a
Begotten-Traveler <solomondev002@gmail.com>
2025-05-01 02:18:02 +0100

    chore: popup and close button 2 added

--- projects/click to expand app/clicktoexpand.js
+++ projects/click to expand app/clicktoexpand.js
@@ -1,15 +1,17 @@
 let buttonEl1 = document.getElementById("btn-1");
 let buttonEl2 = document.getElementById("btn-2");
-let popup = document.getElementById("popup")
+let popup1 = document.getElementById("popup1")
+let popup2 = document.getElementById("popup2")
 
 buttonEl1.addEventListener("click", function(){
-    popup.style.display = "block";
+    popup1.style.display = "block";
 })
 
 buttonEl2.addEventListener("click", function(){
-    popup.style.display = "block"
+    popup2.style.display = "block";
 })
 
 function closePopup(){
-    popup.style.display = "none";
+    popup1.style.display = "none";
+    popup2.style.display = "none";
 }
