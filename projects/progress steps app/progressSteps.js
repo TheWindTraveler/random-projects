bac4bfb8c7549d67184edabc8e1b75d3f0d6e056
Begotten-Traveler <solomondev002@gmail.com>
2025-05-12 11:12:43 +0100

    chore: next function added

--- projects/progress steps app/progressSteps.js
+++ projects/progress steps app/progressSteps.js
@@ -1,4 +1,14 @@
 const progressEl = document.getElementById("progress");
 const prevEl = document.getElementById("prev");
 const nextEl = document.getElementById("next");
-const circleEl = document.getElementById("circle");
+const circleEl = document.querySelectorAll("circle");
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+    currentActive++
+    
+    if(currentActive > circleEl.length){
+        currentActive = circleEl.length
+    }
+});
