7d49ff51678a2e4ad2fc4b1676299ddc2e8fe75d
Begotten-Traveler <solomondev002@gmail.com>
2025-05-12 11:33:37 +0100

    chore: update function done and everything working

--- projects/progress steps app/progressSteps.js
+++ projects/progress steps app/progressSteps.js
@@ -1,7 +1,7 @@
 const progressEl = document.getElementById("progress");
 const prevEl = document.getElementById("prev");
 const nextEl = document.getElementById("next");
-const circleEl = document.querySelectorAll("circle");
+const circleEl = document.querySelectorAll(".circle");
 
 let currentActive = 1;
 
@@ -23,3 +23,25 @@ prevEl.addEventListener('click', () => {
 
     update()
 })
+
+function update(){
+    circleEl.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active')
+        };
+    });
+
+    const activesEl = document.querySelectorAll(".active");
+    progressEl.style.width = (activesEl.length - 1) / (circleEl.length - 1) * 100 + '%';
+
+    if (currentActive === 1){
+        prevEl.disabled = true;
+    } else if (currentActive === circleEl.length){
+        nextEl.disabled = true;
+    } else {
+        prevEl.disabled = false;
+        nextEl.disabled = false;
+    }
+}
